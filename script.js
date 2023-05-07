af111f036a97b313b70061796a489edce7c892ec
Aarti1113 <128404408+Aarti1113@users.noreply.github.com>
2023-05-07 18:54:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".multiple-text", {
+    strings:[ "HARDWORKING", "PASSIONATE","FRONTEND DEVELOPER"],
+    typespeed:500,
+    backspeed:500,
+    backDelay:1000,
+    loop:true
+})
